20207d1c54a01662834566903feb8bfc36851aa2
DanyBuendia <dany.buendia@sirags.com>
2026-07-07 17:01:56 -0600

    modificacion de empleados

--- controllers/employeesControllers.js
+++ controllers/employeesControllers.js
@@ -2,53 +2,102 @@ const db = require('../db/connection');
 const { encryptData } = require('../utils/encrypt');
 
 exports.getEmployees = (req, res) => {
-  db.query('SELECT * FROM employee ORDER BY id DESC', (err, results) => {
-    if (err) return res.status(500).json({ error: err.message || err });
-    res.json(results);
-  });
+    db.query('SELECT * FROM employee ORDER BY id DESC', (err, results) => {
+        if (err) return res.status(500).json({ error: err.message || err });
+        res.json(encryptData(results));
+    });
+};
+
+exports.changeEmployee = (req, res) => {
+    const {
+        id,
+        employeeNumber,
+        position,
+        department,
+        email,
+        phone,
+        address,
+        startDate,
+        emergencyContact,
+        emergencyPhone,
+        status,
+        name,
+        //array certifications,
+        //array overtimeRecords 'tiempo extra'
+    } = req.body;
+
+    db.query(
+        `UPDATE employee SET employeeNumber = ?, position= ?, department = ?, email = ?, 
+        phone = ?, address = ?, startDate = ?, emergencyContact = ?, emergencyPhone = ?, status = ?, name= ?  WHERE id = ?`,
+        [
+            employeeNumber || null,
+            position || null,
+            department || null,
+            email || null,
+            phone || null,
+            address || null,
+            startDate || null,
+            emergencyContact || null,
+            emergencyPhone || null,
+            status || null,
+            name || null,
+            id
+        ],
+        (err, result) => {
+            if (err) return res.status(500).json({ error: error.message || err });
+
+            console.log("modificacion exitosa")
+            console.log(encryptData({ message: "Datos Actualizados" }))
+            res.json(
+                encryptData({
+                    message: "Datos Actualizados"
+                })
+            );
+        }
+    );
 };
 
 exports.addEmployee = (req, res) => {
-  const {
-    employeeNumber,
-    name,
-    position,
-    department,
-    email,
-    phone,
-    address,
-    startDate,
-    birthDate,
-    emergencyContact,
-    emergencyPhone,
-    status
-  } = req.body;
+    const {
+        employeeNumber,
+        name,
+        position,
+        department,
+        email,
+        phone,
+        address,
+        startDate,
+        birthDate,
+        emergencyContact,
+        emergencyPhone,
+        status
+    } = req.body;
 
-  db.query(
-    'INSERT INTO employee (employeeNumber, position, department, email, phone, address, startDate, birthDate, emergencyContact, emergencyPhone, status, name) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-    [
-      employeeNumber || null,
-      position || null,
-      department || null,
-      email || null,
-      phone || null,
-      address || null,
-      startDate || null,
-      birthDate || null,
-      emergencyContact || null,
-      emergencyPhone || null,
-      status || null,
-      name || null
-    ],
-    (err, result) => {
-      if (err) return res.status(500).json({ error: err.message || err });
+    db.query(
+        'INSERT INTO employee (employeeNumber, position, department, email, phone, address, startDate, birthDate, emergencyContact, emergencyPhone, status, name) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+        [
+            employeeNumber || null,
+            position || null,
+            department || null,
+            email || null,
+            phone || null,
+            address || null,
+            startDate || null,
+            birthDate || null,
+            emergencyContact || null,
+            emergencyPhone || null,
+            status || null,
+            name || null
+        ],
+        (err, result) => {
+            if (err) return res.status(500).json({ error: err.message || err });
 
-      res.json(
-        encryptData({
-          message: 'Empleado registrado correctamente',
-          employeeId: result.insertId
-        })
-      );
-    }
-  );
+            res.json(
+                encryptData({
+                    message: 'Empleado registrado correctamente',
+                    employeeId: result.insertId
+                })
+            );
+        }
+    );
 };
--- routes/routesEmployees.js
+++ routes/routesEmployees.js
@@ -5,17 +5,21 @@ const employeesController = require('../controllers/employeesControllers');
 router.get('/', employeesController.getEmployees);
 
 router.post('/', (req, res) => {
-  if (!req.body || !req.body.action) {
-    return res.status(400).json({ error: 'Se requiere el campo action' });
-  }
 
-  switch (req.body.action) {
-    case 'addEmployee':
-      employeesController.addEmployee(req, res);
-      break;
-    default:
-      res.status(400).json({ error: 'Acción no reconocida' });
-  }
+    if (!req.body || !req.body.action) {
+        return res.status(400).json({ error: 'Se requiere el campo action' });
+    }
+    switch (req.body.action) {
+
+        case 'addEmployee':
+            employeesController.addEmployee(req, res);
+            break;
+        case 'updateUserInfo':
+            employeesController.changeEmployee(req, res);
+            break;
+        default:
+            res.status(400).json({ error: 'Acción no reconocida' });
+    }
 });
 
 module.exports = router;
